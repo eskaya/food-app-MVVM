4b6b7516dc1d3076899669d8389a4fd9a55aeaa2
eskaya1 <esra.kaya@naylalabs.com>
2022-12-16 17:39:50 +0300

    Create home page design: wip.

--- app/src/main/java/com/example/food_app_mvvm/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/food_app_mvvm/ui/home/HomeFragment.kt
@@ -5,14 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.food_app_mvvm.R
+import com.example.food_app_mvvm.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
     }
 }
